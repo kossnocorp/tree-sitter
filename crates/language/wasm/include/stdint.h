2e4d056a200fb10c3fce4e5ac16457dc2cc39322
Sasha Koss <koss@nocorp.me>
2025-12-23 22:36:02 +0000

    Add missing conts like UINT8_MAX needed for tree-sitter-python

--- crates/language/wasm/include/stdint.h
+++ crates/language/wasm/include/stdint.h
@@ -23,9 +23,53 @@ typedef long unsigned int size_t;
 
 typedef long unsigned int uintptr_t;
 
-#define UINT16_MAX 65535
+#ifndef INT8_MIN
+#define INT8_MIN (-128)
+#endif
 
+#ifndef INT8_MAX
+#define INT8_MAX 127
+#endif
+
+#ifndef UINT8_MAX
+#define UINT8_MAX 255
+#endif
+
+#ifndef INT16_MIN
+#define INT16_MIN (-32768)
+#endif
+
+#ifndef INT16_MAX
+#define INT16_MAX 32767
+#endif
+
+#ifndef UINT16_MAX
+#define UINT16_MAX 65535
+#endif
+
+#ifndef INT32_MIN
+#define INT32_MIN (-2147483648)
+#endif
+
+#ifndef INT32_MAX
+#define INT32_MAX 2147483647
+#endif
+
+#ifndef UINT32_MAX
 #define UINT32_MAX 4294967295U
+#endif
+
+#ifndef INT64_MIN
+#define INT64_MIN (-9223372036854775807LL - 1)
+#endif
+
+#ifndef INT64_MAX
+#define INT64_MAX 9223372036854775807LL
+#endif
+
+#ifndef UINT64_MAX
+#define UINT64_MAX 18446744073709551615ULL
+#endif
 
 #if defined(__wasm32__)
 
